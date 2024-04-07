a5802a5bd6fdc081c9559d6b5ffc24967ab7348a
daeunleeeee <lde7953@gmail.com>
2024-04-08 00:49:45 +0900

    ✨ [FEAT] global color

---
+++ facemind/lib/utils/global.colors.dart
@@ -0,0 +1,10 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColors {
+  //// main color : A6CA50
+  //// main bg color : FFFFFF
+  //// sub bg color : FCF9F6
+  //// text color : 969696
+  static HexColor mainColor = HexColor('#FFFFFF');
+  static HexColor textColor = HexColor('#969696');
+}
