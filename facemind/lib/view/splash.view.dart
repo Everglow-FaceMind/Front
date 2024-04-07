347bdd375da49247359cc66582bc5bd2eec2402d
daeunleeeee <lde7953@gmail.com>
2024-04-08 00:47:38 +0900

    ✨ [FEAT] splash view

---
+++ facemind/lib/view/splash.view.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:get/get.dart';
+import 'package:facemind/view/login.view.dart';
+import 'package:flutter/material.dart';
+import 'package:facemind/utils/global.colors.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () {
+      Get.to(LoginView());
+    });
+    return Scaffold(
+        backgroundColor: GlobalColors.mainColor,
+        body: const Center(
+            child: Text('FaceMind',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ))));
+  }
+}
