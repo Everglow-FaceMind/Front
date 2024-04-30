011766fc5d1b55565b15ecf13e3d86a9b1e51c37
daeunleeeee <lde7953@gmail.com>
2024-04-30 21:50:27 +0900

    💩 [ADD] 일기 작성 뷰 기능 추가

--- facemind/lib/utils/global_colors.dart
+++ facemind/lib/utils/global_colors.dart
@@ -7,6 +7,7 @@ class GlobalColors {
   //// dark gray color : 969696
   static HexColor mainColor = HexColor('#A6CA50');
   static HexColor lightgreenColor = HexColor('#D5E63E');
+  static HexColor darkgreenColor = HexColor('6A9D1A');
 
   ///초록이
   static HexColor darkgrayColor = HexColor('#969696');
@@ -22,6 +23,7 @@ class GlobalColors {
 
   ///배경, 글씨색,
   static HexColor subBgColor = HexColor('#FCF9F6');
+  static HexColor darkyellow = HexColor('#DDAB63');
 
   ///연노랑
 }
--- facemind/lib/view/diary/new_diary.dart
+++ facemind/lib/view/diary/new_diary.dart
@@ -1,32 +1,29 @@
 import 'package:facemind/utils/global_colors.dart';
+import 'package:facemind/widgets/button_global.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 class NewDiaryView extends StatefulWidget {
-  // 페이지에 표시할 날짜 정보
   final DateTime date;
 
   const NewDiaryView({
     super.key,
     required this.date,
   });
+
   @override
   State<NewDiaryView> createState() => _NewDiaryViewState();
 }
 
 class _NewDiaryViewState extends State<NewDiaryView> {
-  DateTime date = DateTime.now();
+  late DateTime date;
+  List<String> emotions = [];
 
   @override
   void initState() {
     date = widget.date;
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 
   @override
@@ -40,7 +37,7 @@ class _NewDiaryViewState extends State<NewDiaryView> {
           children: [
             Container(
               padding: const EdgeInsets.only(left: 15),
-              child: Text(
+              child: const Text(
                 '일기 작성',
                 style: TextStyle(
                   color: Colors.black,
@@ -55,20 +52,272 @@ class _NewDiaryViewState extends State<NewDiaryView> {
               color: Colors.grey[300],
               thickness: 1,
             ),
-            SizedBox(height: 10),
-
-            //날짜 표시
-
-            Stack(_displayStat, _displayDate),
-
-            // 일기 내용 여기에
+            const SizedBox(height: 10),
+            SizedBox(
+              child: Row(
+                children: [
+                  _displayStat,
+                  const Spacer(),
+                  _displayDate,
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Text(
+                  'Q1 ',
+                  style: TextStyle(
+                    color: GlobalColors.darkgreenColor,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Text(
+                  '나는',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                _buildEmotionInputs(),
+                const Text(
+                  '한 감정을 느꼈다.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  '엔터를 사용해서 등록할 수 있습니다. (최대 3개)\n등록한 값을 터치하면 삭제됩니다.',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: GlobalColors.darkyellow,
+                    fontSize: 10,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Text(
+                  'Q2 ',
+                  style: TextStyle(
+                    color: GlobalColors.darkgreenColor,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Text(
+                  '내 감정의 원인은',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                _buildEmotionInputs(),
+                const Text(
+                  '이다.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  '엔터를 사용해서 등록할 수 있습니다. (최대 3개)\n등록한 값을 터치하면 삭제됩니다.',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: GlobalColors.darkyellow,
+                    fontSize: 10,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Text(
+                  'Q3 ',
+                  style: TextStyle(
+                    color: GlobalColors.darkgreenColor,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '그 일의 객관적인 사실만 묘사해보세요.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+                height: 100,
+                // 캘린더 전체 영역을 감싸는 컨테이너
+                decoration: BoxDecoration(
+                  color: GlobalColors.subBgColor,
+                  borderRadius: BorderRadius.circular(16),
+                )),
+            const SizedBox(height: 25),
+            ButtonGlobal(
+              text: '작성 완료',
+              onPressed: () {},
+              buttonColor: GlobalColors.darkgrayColor,
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget get _displayStat {}
+  Widget _buildEmotionInputs() {
+    List<Widget> emotionWidgets = [];
+
+    for (int i = 0; i < emotions.length; i++) {
+      emotionWidgets.add(
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              emotions.removeAt(i);
+            });
+          },
+          child: Text(
+            emotions[i],
+          ),
+        ),
+      );
+    }
+
+    if (emotions.length < 3) {
+      emotionWidgets.add(
+        GestureDetector(
+          onTap: () {
+            _showInputDialog();
+          },
+          child: const Text(
+            ' ______',
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: emotionWidgets,
+    );
+  }
+
+  void _showInputDialog() {
+    showDialog(
+      barrierColor: Color.fromARGB(162, 161, 161, 161),
+      context: context,
+      builder: (BuildContext context) {
+        String emotion = '';
+        return AlertDialog(
+          backgroundColor: GlobalColors.subBgColor,
+          surfaceTintColor: GlobalColors.whiteColor,
+          title: const Text(
+            '감정 입력',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          content: TextField(
+            cursorColor: GlobalColors.darkgreenColor,
+            onChanged: (value) {
+              emotion = value;
+            },
+            onSubmitted: (value) {
+              if (emotion.isNotEmpty) {
+                setState(() {
+                  emotions.add(emotion);
+                });
+                Navigator.pop(context);
+              }
+            },
+            decoration: const InputDecoration(
+              //textfield 밑줄 호버 시 색 black
+              hintText: '어떤 감정을 느꼈나요?',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('취소',
+                  style: TextStyle(color: GlobalColors.darkgreenColor)),
+            ),
+            TextButton(
+              onPressed: () {
+                if (emotion.isNotEmpty) {
+                  setState(() {
+                    emotions.add(emotion);
+                  });
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('등록',
+                  style: TextStyle(color: GlobalColors.darkgreenColor)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget get _displayStat {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '평균',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+          textAlign: TextAlign.left,
+        ),
+        Text(
+          '최고',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+          textAlign: TextAlign.left,
+        ),
+        Text(
+          '최저',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+          textAlign: TextAlign.left,
+        ),
+        Text(
+          '스트레스 지수',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+          textAlign: TextAlign.left,
+        ),
+      ],
+    );
+  }
+
   Widget get _displayDate {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -76,7 +325,7 @@ class _NewDiaryViewState extends State<NewDiaryView> {
       children: [
         Text(
           '${widget.date.year}',
-          style: Theme.of(context).textTheme.labelSmall?.apply(
+          style: Theme.of(context).textTheme.labelMedium?.apply(
                 color: GlobalColors.mainColor,
               ),
         ),
