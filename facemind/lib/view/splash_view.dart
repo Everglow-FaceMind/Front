108ac6e7675ae49d66848f55c88e38f90608836b
daeunleeeee <lde7953@gmail.com>
2024-05-20 23:57:38 +0900

    ⚡️ [IMPROVE] splash 뷰 텍스트 천천히 나오게

--- facemind/lib/view/splash_view.dart
+++ facemind/lib/view/splash_view.dart
@@ -1,14 +1,68 @@
-import 'dart:async';
-
+import 'package:flutter/material.dart';
 import 'package:facemind/utils/user_store.dart';
 import 'package:facemind/view/home/home_view.dart';
 import 'package:get/get.dart';
 import 'package:facemind/view/login/login_view.dart';
-import 'package:flutter/material.dart';
 import 'package:facemind/utils/global_colors.dart';
 
+class AnimatedTextAppear extends StatefulWidget {
+  final String text;
+  final Duration duration;
+
+  const AnimatedTextAppear({
+    required this.text,
+    required this.duration,
+    super.key,
+  });
+
+  @override
+  _AnimatedTextAppearState createState() => _AnimatedTextAppearState();
+}
+
+class _AnimatedTextAppearState extends State<AnimatedTextAppear>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: _animation.value,
+      child: Text(
+        widget.text,
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
 class SplashView extends StatefulWidget {
-  const SplashView({super.key});
+  const SplashView({Key? key}) : super(key: key);
+
   @override
   State<SplashView> createState() => _SplashViewState();
 }
@@ -25,13 +79,9 @@ class _SplashViewState extends State<SplashView> {
     return Scaffold(
       backgroundColor: GlobalColors.whiteColor,
       body: const Center(
-        child: Text(
-          'FaceMind',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
+        child: AnimatedTextAppear(
+          text: 'FaceMind',
+          duration: Duration(seconds: 2),
         ),
       ),
     );
